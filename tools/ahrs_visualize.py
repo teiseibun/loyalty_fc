d48ddab060cfcd39a635ae51a1572b289ddcfe3d
Tei Seibun <teiseibun.tw@gmail.com>
2019-08-29 01:34:28 +0800

    ahrs_visualize: read quaternion from serial and animate the attitude

--- tools/ahrs_visualize.py
+++ tools/ahrs_visualize.py
@@ -4,12 +4,29 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from pygame.locals import *
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+import argparse
+import threading
+import serial
+import struct
+from collections import deque
+from datetime import datetime
+
+from ahrs_visualize import *
+
+ser = serial.Serial(
+    port='/dev/ttyUSB0',\
+    baudrate=115200,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+    timeout=0)
+
 useQuat = True
 
-q0 = 0.0
-q1 = 0.33
-q2 = 0.33
-q3 = 0.33
+q = [1.0, 0.0, 0.0, 0.0]
 
 def ahrs_visualize_init():
     video_flags = OPENGL | DOUBLEBUF
@@ -29,7 +46,7 @@ def ahrs_visualize_init():
             #[yaw, pitch, roll] = [0, 0, 0]
             draw(1, yaw, pitch, roll)
         else:
-            draw(q0, q1, q2, q3)
+            draw(q[0], q[1], q[2], q[3])
 
         pygame.display.flip()
         frames += 1
@@ -135,3 +152,71 @@ def quat_to_ypr(q):
     yaw   -= -0.13  # Declination at Chandrapur, Maharashtra is - 0 degress 13 min
     roll  *= 180.0 / math.pi
     return [yaw, pitch, roll]
+
+class serial_plotter_class:
+    def serial_receive(self):
+        while ser.inWaiting() > 0:
+            buffer = []
+            checksum = 0
+
+            c = ser.read(1)
+
+            #wait for start byte
+            if c == '@':
+                #print('start byte received')
+                pass
+            else:
+                continue
+
+            #receive package size
+            while ser.inWaiting() == 0:
+                continue
+            payload_count, =  struct.unpack("B", ser.read(1))
+            #print('payload size: %d' %(payload_count))
+
+            #receive message id
+            while ser.inWaiting() == 0:
+                continue
+            _message_id, =  struct.unpack("c", ser.read(1))
+            message_id = ord(_message_id)
+            if message_id != 4:
+                continue
+            print('[%s]received message, id:%d' %(datetime.now().strftime('%H:%M:%S'), message_id))
+
+            #receive payload and calculate checksum
+            for i in range(0, payload_count):
+                while ser.inWaiting() == 0:
+                    continue
+                buffer.append(ser.read(1))
+                buffer_checksum ,= struct.unpack("B", buffer[i])
+                checksum ^= buffer_checksum
+
+            #checksum test
+            received_checksum ,= struct.unpack("B", ser.read())
+            if received_checksum != checksum:
+                    print("error: checksum mismatch");
+                    return 'fail'
+            else:
+                    #print("checksum is correct (%d)" %(checksum))
+                    pass
+
+            for i in range(0, 4):
+                #unpack received data
+                binary_data = ''.join([buffer[i * 4], buffer[i * 4 + 1], buffer[i * 4 + 2], buffer[i * 4 + 3]])
+                float_data = np.asarray(struct.unpack("f", binary_data))
+                q[i] = float_data
+                print("received: %f" %(float_data))
+            #print("-----------------------------");
+            return 'success'
+
+#ahrs_visualize_init()
+serial_plotter = serial_plotter_class()
+
+class serial_thread(threading.Thread):
+	def run(self):
+		while True:
+			serial_plotter.serial_receive()
+
+serial_thread().start()
+
+ahrs_visualize_init()
