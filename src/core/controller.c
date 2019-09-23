a0a9777053083d9416e2ad630fa1626ffe530285
Tei Seibun <teiseibun.tw@gmail.com>
2019-09-23 10:53:20 +0800

    fc: fix yaw rate control sign

--- src/core/controller.c
+++ src/core/controller.c
@@ -12,7 +12,7 @@ void attitude_pd_control(pid_control_t *pid, float ahrs_attitude,
 	//error = reference (setpoint) - measurement
 	pid->error_current = setpoint_attitude - ahrs_attitude;
 
-	pid->error_derivative = angular_velocity;
+	pid->error_derivative = -angular_velocity; //error_derivative = 0 (setpoint) - measurement_derivative
 
 	pid->p_final = pid->kp * pid->error_current;
 	pid->d_final = pid->kd * pid->error_derivative;
@@ -25,8 +25,7 @@ void attitude_pd_control(pid_control_t *pid, float ahrs_attitude,
 
 void yaw_rate_p_control(pid_control_t *pid, float setpoint_yaw_rate, float angular_velocity)
 {
-	//pid->error_current = setpoint_yaw_rate - angular_velocity;
-	pid->error_current = setpoint_yaw_rate;
+	pid->error_current = setpoint_yaw_rate - angular_velocity;
 
 	pid->p_final = pid->kp * pid->error_current;
 	pid->output = pid->p_final;
@@ -47,10 +46,15 @@ void motor_control(volatile float throttle_percentage, float roll_ctrl_precentag
 
 	float m1_pwm, m2_pwm, m3_pwm, m4_pwm;
 
-	m1_pwm = power_basis + roll_pwm + pitch_pwm + yaw_pwm;
-	m2_pwm = power_basis - roll_pwm + pitch_pwm - yaw_pwm;
-	m3_pwm = power_basis - roll_pwm - pitch_pwm + yaw_pwm;
-	m4_pwm = power_basis + roll_pwm - pitch_pwm - yaw_pwm;
+	/* (ccw)    (cw)
+	    m2       m1
+	         x 
+	    m3       m4
+	   (cw)    (ccw) */
+	m1_pwm = power_basis + roll_pwm + pitch_pwm - yaw_pwm;
+	m2_pwm = power_basis - roll_pwm + pitch_pwm + yaw_pwm;
+	m3_pwm = power_basis - roll_pwm - pitch_pwm - yaw_pwm;
+	m4_pwm = power_basis + roll_pwm - pitch_pwm + yaw_pwm;
 	bound_float(&m1_pwm, MOTOR_PULSE_MAX, MOTOR_PULSE_MIN);
 	bound_float(&m2_pwm, MOTOR_PULSE_MAX, MOTOR_PULSE_MIN);
 	bound_float(&m3_pwm, MOTOR_PULSE_MAX, MOTOR_PULSE_MIN);
